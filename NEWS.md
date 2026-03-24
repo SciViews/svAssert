80c9526d3f032b6bd9caa6651874cb6a736cd06a
Philippe Grosjean <phgrosjean@sciviews.org>
2026-03-24 12:21:38 +0100

    any_infinite() only parallelized if the size of the vector is >= 1 million items

--- NEWS.md
+++ NEWS.md
@@ -1,3 +1,9 @@
+# svAssert 0.1.1
+
+*   Starting the development of fast, parallelized C code for several checks. `get_threads()`/`set_threads()` functions to control the number of threads used by the C code. The default is 1 thread for non parallelized code.
+
+*    `any_infinite(` similar to `checkmate::anyInfinite()`, but using parallel code for large vectors.
+
 # svAssert 0.1.0
 
-* Initial version.
+*   Initial version.
--- src/any_finite.c
+++ src/any_finite.c
@@ -1,23 +1,34 @@
 #include "any_finite.h"
 #include "threads.h"
 
+const R_xlen_t n_min_para = 1e6;
+
 static Rboolean any_infinite_double(SEXP x) {
     const double *xp = REAL_RO(x);
     const R_xlen_t n = xlength(x);
     int found = 0;
 
-    #pragma omp parallel for num_threads(n_threads) shared(found) schedule(static)
-    for (R_xlen_t i = 0; i < n; i++) {
-        int local_found;
-        #pragma omp atomic read
-        local_found = found;
-        if (local_found) continue;
-        if (xp[i] == R_PosInf || xp[i] == R_NegInf) {
-            #pragma omp atomic write
-            found = 1;
+    if (n_threads > 1 && n >= n_min_para){
+        #pragma omp parallel for num_threads(n_threads) shared(found) schedule(static)
+        for (R_xlen_t i = 0; i < n; i++) {
+            int local_found;
+            #pragma omp atomic read
+            local_found = found;
+            if (local_found) continue;
+            if (xp[i] == R_PosInf || xp[i] == R_NegInf) {
+               #pragma omp atomic write
+               found = 1;
+            }
+        }
+    } else {
+        for (R_xlen_t i = 0; i < n; i++) {
+            if (xp[i] == R_PosInf || xp[i] == R_NegInf) {
+               found = 1;
+               break;
+            }
         }
     }
-
+    
     return found ? TRUE : FALSE;
 }
 
@@ -26,16 +37,26 @@ static Rboolean any_infinite_complex(SEXP x) {
     const R_xlen_t n = xlength(x);
     int found = 0;
 
-    #pragma omp parallel for num_threads(n_threads) shared(found) schedule(static)
-    for (R_xlen_t i = 0; i < n; i++) {
-        int local_found;
-        #pragma omp atomic read
-        local_found = found;
-        if (local_found) continue;
-        if (xp[i].r == R_PosInf || xp[i].r == R_NegInf ||
-            xp[i].i == R_PosInf || xp[i].i == R_NegInf) {
-            #pragma omp atomic write
-            found = 1;
+    if (n_threads > 1 && n >= n_min_para){
+        #pragma omp parallel for num_threads(n_threads) shared(found) schedule(static)
+        for (R_xlen_t i = 0; i < n; i++) {
+            int local_found;
+            #pragma omp atomic read
+            local_found = found;
+            if (local_found) continue;
+            if (xp[i].r == R_PosInf || xp[i].r == R_NegInf ||
+                xp[i].i == R_PosInf || xp[i].i == R_NegInf) {
+                #pragma omp atomic write
+                found = 1;
+            }
+        }
+    } else {
+        for (R_xlen_t i = 0; i < n; i++) {
+            if (xp[i].r == R_PosInf || xp[i].r == R_NegInf ||
+                xp[i].i == R_PosInf || xp[i].i == R_NegInf) {
+               found = 1;
+               break;
+            }
         }
     }
 
